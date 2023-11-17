e812cc373c56a18f16effd8472147b3e58d9b22d
JahnaviSalammagari <jahnavireddy.salammagari@gmail.com>
2023-11-17 09:19:18 -0800

    fetch products from backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,32 @@
-import React from 'react'
-import './bootstrap.min.css'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-<React.StrictMode>
-  <App />
-</React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
+    </Route>
+  )
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+reportWebVitals();
